68d7db9840d584a06729edf27b8b22f56957ecf3
jnpalmstrom <jnpalmstrom@gmail.com>
2019-02-15 12:13:52 -0500

    Hopefully hash works

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 let LocalStrategy = require('passport-local').Strategy;
 let SamlStrategy = require('passport-saml').Strategy;
 let mysql = require('mysql');
+let bcrypt = require('bcrypt');
 
 let connection = mysql.createConnection({
     host: 'snapdispatcherdb.ca40maoxylrp.us-east-1.rds.amazonaws.com',
@@ -91,24 +92,27 @@ module.exports = function(passport, config) {
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
         function(req, username, password, done) { // callback with email and password from our form
+            // Load hash from your password DB.
+            bcrypt.compare(req.body.password, hash, function(err, res) {
+                // res == true
+                let loginStatement = "SELECT * FROM superUsers WHERE username = ? AND password = ?";
+                let loginInfo = [req.body.username, req.body.password];
+                connection.query(loginStatement, loginInfo, function(err,rows){
+                    if (err)
+                        return done(err);
+                    if (!rows.length) {
+                        return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                    }
 
-            let loginStatement = "SELECT * FROM superUsers WHERE username = ?";
-            let loginUsername = [req.body.username];
+                    // if the user is found but the password is wrong
+                    if (!(rows[0].password === password))
+                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-            connection.query(loginStatement, loginUsername, function(err,rows){
-                if (err)
-                    return done(err);
-                if (!rows.length) {
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-                }
-
-                // if the user is found but the password is wrong
-                if (!(rows[0].password === password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
-                // all is well, return successful user
-                return done(null, rows[0]);
+                    // all is well, return successful user
+                    return done(null, rows[0]);
+                });
             });
+
         }));
 
     ////////////////////////////////////////////////////////////
