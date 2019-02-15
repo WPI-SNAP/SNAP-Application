9680bda17490cdd806f1e16f0a512313041ce707
jnpalmstrom <jnpalmstrom@gmail.com>
2019-02-15 12:17:41 -0500

    Added things...

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ let connection = mysql.createConnection({
 });
 
 // expose this function to our app using module.exports
-module.exports = function(passport, config) {
+module.exports = function (passport, config) {
 
     // =========================================================================
     // passport session setup ==================================================
@@ -21,16 +21,16 @@ module.exports = function(passport, config) {
     // passport needs ability to serialize and unserialize users out of session
 
     // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
+    passport.serializeUser(function (user, done) {
         done(null, user.username);
     });
 
     // used to deserialize the user
-    passport.deserializeUser(function(username, done) {
+    passport.deserializeUser(function (username, done) {
 
         let deserializeStmt = "SELECT * FROM superUsers WHERE username = ?";
 
-        connection.query(deserializeStmt, username, function(err,rows){
+        connection.query(deserializeStmt, username, function (err, rows) {
             done(err, rows[0]);
         });
     });
@@ -44,15 +44,15 @@ module.exports = function(passport, config) {
 
     passport.use('local-signup', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) {
+        function (req, username, password, done) {
 
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            connection.query("select * from users where username = '" + username + "'",function(err,rows){
+            connection.query("select * from users where username = '" + username + "'", function (err, rows) {
                 console.log(rows);
                 console.log("above row object");
                 if (err)
@@ -68,9 +68,9 @@ module.exports = function(passport, config) {
                     newUserMysql.username = username;
                     newUserMysql.password = password; // use the generateHash function in our user model
 
-                    var insertQuery = "INSERT INTO superUsers (username, password) values ('" + username +"','"+ password +"')";
+                    var insertQuery = "INSERT INTO superUsers (username, password) values ('" + username + "','" + password + "')";
                     console.log(insertQuery);
-                    connection.query(insertQuery,function(err,rows){
+                    connection.query(insertQuery, function (err, rows) {
                         newUserMysql.id = rows.insertId;
 
                         return done(null, newUserMysql);
@@ -87,30 +87,27 @@ module.exports = function(passport, config) {
 
     passport.use('local-login', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) { // callback with email and password from our form
-            // Load hash from your password DB.
-            bcrypt.compare(req.body.password, hash, function(err, res) {
-                // res == true
-                let loginStatement = "SELECT * FROM superUsers WHERE username = ? AND password = ?";
-                let loginInfo = [req.body.username, req.body.password];
-                connection.query(loginStatement, loginInfo, function(err,rows){
-                    if (err)
-                        return done(err);
-                    if (!rows.length) {
-                        return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-                    }
+        function (req, username, password, done) { // callback with email and password from our form
+            // TODO: Add bcrypt compare here
+            let loginStatement = "SELECT * FROM superUsers WHERE username = ? AND password = ?";
+            let loginInfo = [req.body.username, req.body.password];
+            connection.query(loginStatement, loginInfo, function (err, rows) {
+                if (err)
+                    return done(err);
+                if (!rows.length) {
+                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                }
 
-                    // if the user is found but the password is wrong
-                    if (!(rows[0].password === password))
-                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                // if the user is found but the password is wrong
+                if (!(rows[0].password === password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-                    // all is well, return successful user
-                    return done(null, rows[0]);
-                });
+                // all is well, return successful user
+                return done(null, rows[0]);
             });
 
         }));
--- config/routes.js
+++ config/routes.js
@@ -92,8 +92,7 @@ module.exports = function (app, config, passport) {
         failureRedirect: '/superLogin',
         failureFlash: true
     }));
-
-    //TODO: Create Admin dashboard
+    
     app.get('/superHome', function (req, res) {
         if(!req.isAuthenticated()) res.redirect('/');
         else {
