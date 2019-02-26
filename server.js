3b2af51e07aedef0d6ad12c3c16a708693bb4889
jnpalmstrom <jnpalmstrom@gmail.com>
2019-02-25 20:09:04 -0500

    Changed port num

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 let path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
